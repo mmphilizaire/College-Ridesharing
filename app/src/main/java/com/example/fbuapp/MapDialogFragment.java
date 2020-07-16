082b755b8da42b18cb57d5c18f3c4c124408f4eb
Mishka Philizaire <mishka@fb.com>
2020-07-16 12:59:22 -0400

    User can search for a location to start their ride using Google Maps.

--- app/src/main/java/com/example/fbuapp/MapDialogFragment.java
+++ app/src/main/java/com/example/fbuapp/MapDialogFragment.java
@@ -3,12 +3,19 @@ package com.example.fbuapp;
 import android.Manifest;
 import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -26,8 +33,13 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapDialogFragment extends DialogFragment implements OnMapReadyCallback {
 
@@ -40,7 +52,9 @@ public class MapDialogFragment extends DialogFragment implements OnMapReadyCallb
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
 
-    public static MapDialogFragment newInstance(){
+    private EditText mSearchEditText;
+
+    public static MapDialogFragment newInstance() {
         return new MapDialogFragment();
     }
 
@@ -54,8 +68,9 @@ public class MapDialogFragment extends DialogFragment implements OnMapReadyCallb
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dialog_map, container, false);
+        mSearchEditText = (EditText) view.findViewById(R.id.etSearch);
 
-        if(isServicesOK()){
+        if (isServicesOK()) {
             getLocationPermission();
         }
 
@@ -66,8 +81,14 @@ public class MapDialogFragment extends DialogFragment implements OnMapReadyCallb
     public void onMapReady(GoogleMap googleMap) {
         Toast.makeText(getContext(), "Map is Ready", Toast.LENGTH_SHORT).show();
         mMap = googleMap;
-        if(mLocationPermissionsGranted){
+        if (mLocationPermissionsGranted) {
             getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
         }
     }
 
@@ -90,6 +111,36 @@ public class MapDialogFragment extends DialogFragment implements OnMapReadyCallb
         SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(MapDialogFragment.this);
+
+        mSearchEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH ||
+                actionId == EditorInfo.IME_ACTION_DONE ||
+                keyEvent.getAction() == KeyEvent.ACTION_DOWN ||
+                keyEvent.getAction() == KeyEvent.KEYCODE_ENTER ){
+                    geoLocate();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void geoLocate(){
+        String searchString = mSearchEditText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(getContext());
+        List<Address> list = new ArrayList<>();
+        try{
+            list = geocoder.getFromLocationName(searchString, 1);
+        }catch (IOException e){
+
+        }
+        if(list.size() > 0){
+            Address address = list.get(0);
+            Toast.makeText(getContext(), "Found Location", Toast.LENGTH_SHORT).show();
+            moveCamera(new LatLng(address.getLatitude(), address.getLongitude()), 15f, address.getAddressLine(0));
+        }
     }
 
     private void getLocationPermission(){
@@ -101,6 +152,7 @@ public class MapDialogFragment extends DialogFragment implements OnMapReadyCallb
             if(ContextCompat.checkSelfPermission(getContext(),
                     COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
                 mLocationPermissionsGranted = true;
+                initializeMap();
             }else{
                 ActivityCompat.requestPermissions(getActivity(),
                         permissions,
@@ -144,7 +196,7 @@ public class MapDialogFragment extends DialogFragment implements OnMapReadyCallb
                     public void onComplete(@NonNull Task task) {
                         if(task.isSuccessful()){
                             Location currentLocation = (Location) task.getResult();
-                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), 15f);
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), 15f, "My location");
                         } else{
                             Toast.makeText(getContext(), "Unable to get location", Toast.LENGTH_SHORT).show();
                         }
@@ -157,8 +209,14 @@ public class MapDialogFragment extends DialogFragment implements OnMapReadyCallb
 
     }
 
-    private void moveCamera(LatLng latLng, float zoom){
+    private void moveCamera(LatLng latLng, float zoom, String title){
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+
+        if(!title.equals("My location")){
+            MarkerOptions options = new MarkerOptions().position(latLng).title(title);
+            mMap.addMarker(options);
+        }
+
     }
 
 }
