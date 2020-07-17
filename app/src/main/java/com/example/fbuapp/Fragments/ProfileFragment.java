6cf6063e6e767b7f14e8261e865d6ff167aa7695
Mishka Philizaire <mishka@fb.com>
2020-07-17 19:54:27 -0400

    Add about section to user profile fragment.

--- app/src/main/java/com/example/fbuapp/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/fbuapp/Fragments/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.fbuapp.Fragments;
 
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +14,14 @@ import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.example.fbuapp.Models.RideOffer;
 import com.example.fbuapp.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class ProfileFragment extends Fragment {
 
@@ -23,6 +30,9 @@ public class ProfileFragment extends Fragment {
     private ImageView mProfilePictureImageView;
     private TextView mNameTextView;
     private TextView mUniversityTextView;
+    private TextView mMemberSinceTextView;
+    private TextView mRidesDrivenTextView;
+    private TextView mRidesRiddenTextView;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -42,11 +52,25 @@ public class ProfileFragment extends Fragment {
         mProfilePictureImageView = view.findViewById(R.id.ivProfilePicture);
         mNameTextView = view.findViewById(R.id.tvName);
         mUniversityTextView = view.findViewById(R.id.tvUniversity);
+        mMemberSinceTextView = view.findViewById(R.id.tvMemberSince);
+        mRidesDrivenTextView = view.findViewById(R.id.tvRidesDriven);
+        mRidesRiddenTextView = view.findViewById(R.id.tvRidesRidden);
 
         Glide.with(getContext()).load(user.getParseFile("profilePicture").getUrl()).transform(new CircleCrop()).into(mProfilePictureImageView);
-        mNameTextView.setText(user.getString("firstName")+" "+user.getString("lastName"));
+        mNameTextView.setText(user.getString("firstName")+" "+user.getString("lastName").substring(0,1)+".");
         mUniversityTextView.setText(user.getString("university"));
-
+        mMemberSinceTextView.setText("Member since "+DateFormat.format("MMMM yyyy", user.getCreatedAt()));
+        mRidesDrivenTextView.setText("Driven " + ridesDrivenCount(user) + " Rides");
+        mRidesRiddenTextView.setText("Ridden " + ridesRiddenCount(user) + " Rides");
     }
+
+    private int ridesRiddenCount(ParseUser user) {
+        return 0;
+    }
+
+    private int ridesDrivenCount(ParseUser user) {
+        return 0;
+    }
+
 
 }
